87e8af795ab90bda8e04f3d81fc197eab93dd51c
EthanSkelton9 <bigeskelly@gmail.com>
2024-09-04 14:13:52 -0600

    starting todo list

---
+++ todolist.py
@@ -0,0 +1,22 @@
+
+
+list = []
+
+
+user_input = None
+while user_input != 'q':
+    user_input = input("Press 1 to add items to list \n Press 2 to delete items \n Press 3 to see the list \n (q to quit)")
+
+    if user_input == 1:
+        #add item:
+        pass
+
+    elif user_input == 2:
+        #delete item
+        pass
+
+    elif user_input == 3:
+        #print out the list
+        pass
+
+    
